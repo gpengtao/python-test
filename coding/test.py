a0c2f724be9cc72d2752e2b4642d0b4f1e864c67
耿鹏涛 <pengtao.geng@wormpex.com>
2017-10-26 23:46:34 +0800

    add

--- coding/test.py
+++ coding/test.py
@@ -45,5 +45,27 @@ arr.append(-1)
 arr.append(2)
 print arr
 
+print 'range: %s' % range(0, 10, 3)
+
 r = random.Random()
-print 'random: %d' % r.randint(1, 100)
+print 'random int: %d' % r.randint(1, 100)
+print 'random range: %d' % r.randrange(2, 10, 5)
+print 'random random: %s' % r.random()
+print 'random uniform: %s' % r.uniform(1, 100)
+print 'random choice: %s' % r.choice(range(0, 10, 1))
+
+seq = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print len(seq)
+print seq[:]
+print seq[::]
+print seq[::-1]
+print seq[1]
+print seq[1:3]
+print seq[3:]
+print seq[3:100]
+print seq[0:3]
+print seq[:3]
+print 'type: %s' % type(seq)
+print seq[-1:3]  # 倒着数，但是要超过右边那个下标
+seq2 = seq[::-1]
+print 'reverse: %s' % seq2
